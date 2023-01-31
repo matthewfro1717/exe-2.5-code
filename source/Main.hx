c2ab649bb7ef445d59ecf10f386f7f2e40bab966
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-01-30 20:20:27 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -74,8 +74,6 @@ class Main extends Sprite
 		// FlxGraphic.defaultPersist = true;
 		
 		//
-		
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
